b2ea382bfbda9c0fc3463beeabeb2958ed199fac
刘鹏搏 <liupengbo@beadwallet.com>
2019-04-22 15:26:42 +0800

    springboot admin

--- configclient/src/main/java/com/my/configclient/controller/ConfigController.java
+++ configclient/src/main/java/com/my/configclient/controller/ConfigController.java
@@ -1,5 +1,6 @@
-package com.my.controller;
+package com.my.configclient.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author liu peng bo
  * @date 2019/4/20
  */
+@Slf4j
 @RestController
 public class ConfigController {
     @Autowired
@@ -26,6 +28,7 @@ public class ConfigController {
 
     @GetMapping("/k1")
     public String getK1() {
+        log.info("get value!");
         return k1;
     }
 }
